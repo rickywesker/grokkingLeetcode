214bb9e6bd7dcc8455e7ef3915f1022de52d4a28
Rickycs <rickycs97@pku.edu.cn>
2021-03-18 16:52:41 +0800

    Create Non-repeating.py
    Non-repeating with sliding win method

---
+++ code/SlidingWin/Non-repeating.py
@@ -0,0 +1,15 @@
+def non_repeat_substring(str1):
+    win_start = 0
+    alpha_index = {}
+    max_length = 0
+    for win_end in range(len(str1)):
+        right_char = str1[win_end]
+        #update the start_pointer
+        if right_char in alpha_index:
+            win_start = max(win_start,alpha_index[right_char]+1)
+        #insert the current char INDEX into the dict
+        alpha_index[right_char] = win_end
+        max_length = max(max_length,win_end-win_start+1)
+    return max_length
+
+print(non_repeat_substring("abcdaaa"))
