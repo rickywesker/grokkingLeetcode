3e2accf831c2f6afa83c28081877fa9ca507244b
Rickycs <rickycs97@pku.edu.cn>
2021-03-18 18:27:45 +0800

    another ex

--- code/SlidingWin/longest-repeating-substr-with-replacement.py
+++ code/SlidingWin/longest-repeating-substr-with-replacement.py
@@ -16,7 +16,23 @@ def longest_repeating_substr_replace(str1,k):
             freq_dict[left_char] -= 1
             win_start += 1 #shrinking the win ahead
 
-        max_length = max(max_length,win_end - win_start + 1)
-    return max_length
+        max_length = max(max_length,win_end - win_start + 1)#直接计算窗口长度作为答案，因为符合要求
 
+    return max_length
+def length_of_longest_substring(arr, k):
+      # TODO: Write your code here
+    win_start, ones_cnt, max_length = 0, 0, 0
+    for win_end in range(len(arr)):
+        if arr[win_end] == 1:
+            ones_cnt += 1
+        
+        
+        if win_end - win_start + 1 - ones_cnt > k:
+            if arr[win_start] == 1:
+                ones_cnt -= 1
+        win_start += 1
+        
+        max_length = max(max_length,win_end - win_start + 1)
+    
+    return max_length
 print(longest_repeating_substr_replace("abbcb",0))
