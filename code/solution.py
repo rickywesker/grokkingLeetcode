476d2d69f755237e921bc1b9e9c9f9a0d3bdfad3
rickywesker <rickycs97@pku.edu.cn>
2022-10-24 18:36:54 +0800

    add fib_v3

--- code/solution.py
+++ code/solution.py
@@ -1,4 +1,4 @@
-class Solution:
+class Soluton:
     def __init__(self):
         self.table = [0,1]
     def fib(self,n):
@@ -8,9 +8,6 @@ class Solution:
         return self.table[n]
     
     def make_table(self,n):
-        if (n == 0 or n == 1):
-            return True
         for i in range(2,n+1):
             self.table.append(self.table[i-1]+self.table[i-2])
-        return True 
 
